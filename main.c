16aabdfdf3581db82dcea7702b65bbd0809111c0
unknown <dincydmv@gmail.com>
2026-02-16 21:18:11 +0530

    added a function

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #define MAX 50
 
 struct Task
@@ -29,7 +30,7 @@ void energyPlanner();
 void addToDoList();
 void getMotivation();
 
-void main()
+int main()
 {
     int choice;
     do
@@ -49,24 +50,75 @@ void main()
         case 1:
             decideNextTask();
             break;
-        case 2:
-            resolveDilemma();
-            break;
-        case 3:
-            energyPlanner();
-            break;
-        case 4:
-            addToDoList();
-            break;
-        case 5:
-            getMotivation();
-            break;
-        case 6:
-            printf("Exiting Program...\n");
-            break;
-        default:
-            printf("Invalid choice\n");
+            // case 2:
+            //     resolveDilemma();
+            //     break;
+            // case 3:
+            //     energyPlanner();
+            //     break;
+            // case 4:
+            //     addToDoList();
+            //     break;
+            // case 5:
+            //     getMotivation();
+            //     break;
+            // case 6:
+            //     printf("Exiting Program...\n");
+            //     break;
+            // default:
+            //     printf("Invalid choice\n");
         }
 
-    } while (choice > 6);
+    } while (choice != 6);
+    return 0;
+}
+
+/*decide what to do next*/
+void decideNextTask()
+{
+    struct Task tasks[MAX];
+    int n, i, j;
+
+    printf("\n Enter number of tasks:");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        printf("\n Enter %d task name: ", i + 1);
+        scanf(" %[^\n]", tasks[i].name);
+
+        printf("\n Importance of the task (1-5): ");
+        scanf("%d", &tasks[i].importance);
+
+        printf("\n Deadline (days left): ");
+        scanf("%d", &tasks[i].deadline);
+    }
+    for (i = 0; i < n - 1; i++)
+    {
+        for (j = 0; j < n - i - 1; j++)
+        {
+            if (tasks[j].importance < tasks[j + 1].importance ||
+                (tasks[j].importance == tasks[j + 1].importance &&
+                 tasks[j].deadline > tasks[j + 1].deadline))
+            {
+
+                struct Task temp = tasks[j];
+                tasks[j] = tasks[j + 1];
+                tasks[j + 1] = temp;
+            }
+        }
+    }
+
+    printf("\n----- GENERATED TIMETABLE -----\n");
+    for (i = 0; i < n; i++)
+    {
+        printf("Task: %s (Importance: %d, Deadline: %d days)\n",
+               tasks[i].name, tasks[i].importance, tasks[i].deadline);
+
+        printf("  -> Work for 1 hour\n");
+        printf("  -> Take 15 min break\n");
+
+        if ((i + 1) % 3 == 0)
+            printf("  -> Take longer 30 min break\n");
+    }
 }
